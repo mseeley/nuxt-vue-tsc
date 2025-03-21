e1e94482f4c81b88dab5c2cbf1c6e6a750e58d9c
mseeley <git@stackblitz.com>
2025-03-20 16:34:34 -1000

    Initial commit

---
+++ components/All.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts" generic="T">
+import { useKeyOf, type KeyOfInput } from '../composables/use-key-of';
+
+export interface AllProps<T> {
+  items?: T[];
+  keyOf?: KeyOfInput<T>;
+}
+
+const props = defineProps<AllProps<T>>();
+const keyOf = useKeyOf(props);
+</script>
+
+<template>
+  <div>
+    <template v-for="(item, index) in props.items" :key="keyOf(item, index)">
+      <slot name="item" :item="item" :item-key="keyOf(item, index)" />
+    </template>
+  </div>
+</template>
