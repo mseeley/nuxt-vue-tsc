13486eb26b62457ccb784e207f826ad3c08693e4
Matt Seeley <mattseeley@hotmail.com>
2025-03-20 18:22:49 -1000

    Tidy

--- apps/web/app.vue
+++ apps/web/app.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-import All from './components/All.vue';
+import All from "./components/All.vue";
 
 const scientists = [
-  { id: '0', name: 'Albert Einstein' },
-  { id: '1', name: 'Marie Curie' },
-  { id: '2', name: 'Isaac Newton' },
+  { id: "0", name: "Albert Einstein" },
+  { id: "1", name: "Marie Curie" },
+  { id: "2", name: "Isaac Newton" },
 ];
 </script>
 
 <template>
   <ul>
     <All :items="scientists" :key-of="(item) => item.id">
-      <template #item="{ item, itemKey }">
-        <li>{{ item.name }} ({{ itemKey }})</li>
+      <template #item="{ item, key }">
+        <li>{{ item.name }} ({{ key }})</li>
       </template>
     </All>
   </ul>
--- apps/web/components/All.vue
+++ apps/web/components/All.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts" generic="T">
-import { useKeyOf, type KeyOfInput } from '../composables/use-key-of';
+import { useKeyOf, type KeyOfInput } from "../composables/use-key-of";
 
 export interface AllProps<T> {
   items?: T[];
@@ -11,9 +11,10 @@ const keyOf = useKeyOf(props);
 </script>
 
 <template>
-  <div>
-    <template v-for="(item, index) in props.items" :key="keyOf(item, index)">
-      <slot name="item" :item="item" :item-key="keyOf(item, index)" />
-    </template>
-  </div>
+  <slot
+    v-for="(item, index) in props.items"
+    :item="item"
+    :key="keyOf(item, index)"
+    name="item"
+  />
 </template>
